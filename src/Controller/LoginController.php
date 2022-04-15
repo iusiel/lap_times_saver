7048cf6dd3bd26978dd09efc79bbedade4a47f11
Iusiel <iusiel@outlook.com>
2022-04-15 09:52:02 +0800

    Login form

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $user = $this->getUser();
+        if (!empty($user)) return $this->redirectToRoute('app_game'); //redirect to game dashboard if user is already logged in
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $plaintextPassword = '123456';
+    
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $user->setEmail('iusiel@outlook.com');
+        $user->setRoles($user->getRoles());
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response('Success');
+    }
+
+
+    
+
+
+}
