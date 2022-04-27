6a9ba90a171dc3000a7d81a0c6e3345704758112
Iusiel <iusiel@outlook.com>
2022-04-27 21:46:43 +0800

    PHPUnit test for login

---
+++ tests/LoginTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\UrlHelper;
+
+class LoginTest extends WebTestCase
+{
+    public function testRedirectToLoginPageIfUserIsNotYetLoggedIn(): void
+    {
+        $client = static::createClient();
+        self::bootKernel();
+        $container = static::getContainer();
+        $urlGenerator = $container->get(UrlHelper::class);
+        
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseRedirects($urlGenerator->getAbsoluteUrl('/login'));
+    }
+
+    public function testSuccessfulLogin()
+    {
+        $client = static::createClient();
+        self::bootKernel();
+        $container = static::getContainer();
+        $urlGenerator = $container->get(UrlHelper::class);
+
+        $client->request('GET', '/login');
+        $crawler = $client->submitForm('Login', [
+            '_username' => 'sample_user@localhost',
+            '_password' => '123456',
+        ]);
+
+        $this->assertResponseRedirects($urlGenerator->getAbsoluteUrl('/'));
+    }
+
+    public function testFailedLogin()
+    {
+        $client = static::createClient();
+        self::bootKernel();
+        $container = static::getContainer();
+        $urlGenerator = $container->get(UrlHelper::class);
+
+        $client->request('GET', '/login');
+        $crawler = $client->submitForm('Login', [
+            '_username' => 'sample_user@localhost',
+            '_password' => '12345677',
+        ]);
+        $this->assertResponseRedirects($urlGenerator->getAbsoluteUrl('/login'));
+
+        $crawler = $client->followRedirect();
+        $errorMessage = $crawler->filter('.alert-danger')->first()->text();
+        $this->assertEquals($errorMessage, 'Invalid credentials.');
+    }
+}
