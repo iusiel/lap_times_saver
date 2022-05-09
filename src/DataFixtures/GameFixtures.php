b4c0e3ddf35af3f680c8de3cd59ba88aedf74464
Iusiel <iusiel@outlook.com>
2022-05-09 21:36:35 +0800

    Game fixtures for dummy data

---
+++ src/DataFixtures/GameFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Game;
+use App\Repository\GameRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\ByteString; 
+
+class GameFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($k=0; $k<=5; $k++) {
+            $game = new Game();
+            $game->setName(ByteString::fromRandom(20)->toString());
+            $game->setCreatedAt(new \DateTime());
+            $game->setUpdatedAt(new \DateTime());
+            $manager->persist($game);
+        }
+
+        $manager->flush();
+    }
+}
