0b17c67f64f82ccd3ed3c62d79e23361c6e02578
Iusiel <iusiel@outlook.com>
2022-05-10 22:02:41 +0800

    Updated sweet alert layout

--- assets/js/services/SweetAlertOnDeleteAction.js
+++ assets/js/services/SweetAlertOnDeleteAction.js
@@ -6,7 +6,16 @@ if (deleteForms.length > 0) {
         form.addEventListener('submit', (event) => {
             event.preventDefault();
             swal("Are you sure you want to do this?", {
-                buttons: ["Cancel", "Yes"],
+                buttons: {
+                    cancel: {
+                        text: "Cancel",
+                        visible: true,
+                        className: 'btn btn-secondary'
+                    },
+                    confirm: {
+                        className: 'btn btn-primary'
+                    },
+                  }
             }).then((value) => {
                 if (value === true) {
                     form.submit();
