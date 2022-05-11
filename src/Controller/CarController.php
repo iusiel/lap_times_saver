f7728efecb0476fab1fd39d465eb536206eb1c6b
Iusiel <iusiel@outlook.com>
2022-05-11 21:41:42 +0800

    Only allow admin access to cars dashboard

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Car;
 use App\Form\CarType;
 use App\Repository\CarRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_ADMIN')]
 #[Route('/car')]
 class CarController extends AbstractController
 {
