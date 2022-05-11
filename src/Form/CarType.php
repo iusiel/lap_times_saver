05c44cf8c308085ee41e276c549b3ba319bd78ad
Iusiel <iusiel@outlook.com>
2022-05-11 21:23:39 +0800

    Initial car form

---
+++ src/Form/CarType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CarType extends AbstractType
+{
+    private $kernelInterface;
+
+    public function __construct(string $env)
+    {
+        $this->kernelInterface = $env;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 255,
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+            'csrf_protection' => ($this->kernelInterface === 'test') ? false : true,
+        ]);
+    }
+}
