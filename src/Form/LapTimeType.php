3fc9427093d1e014fe06d85111f1c73fccf7171a
Iusiel <iusiel@outlook.com>
2022-05-13 20:40:25 +0800

    Initial form for lap time

---
+++ src/Form/LapTimeType.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use App\Entity\Game;
+use App\Entity\LapTime;
+use App\Entity\Track;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class LapTimeType extends AbstractType
+{
+    private $kernelInterface;
+
+    public function __construct(string $env)
+    {
+        $this->kernelInterface = $env;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Date', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'attr' => [
+                    'class' => 'form-control',
+                    'value' => date("Y-m-d")
+                ],
+            ])
+            ->add('Game', EntityType::class, [
+                'class' => Game::class,
+                'choice_label' => 'Name',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.Name', 'ASC');
+                },
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+            ])
+            ->add('Car', EntityType::class, [
+                'class' => Car::class,
+                'choice_label' => 'Name',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.Name', 'ASC');
+                },
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+            ])
+            ->add('Track', EntityType::class, [
+                'class' => Track::class,
+                'choice_label' => 'Name',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.Name', 'ASC');
+                },
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+            ])
+            ->add('Time', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 255,
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ],
+            ])
+            ->add('IsPractice', ChoiceType::class, [
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+                'choices' => [
+                    'Yes' => 1,
+                    'No' => 0,
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ],
+            ])
+            ->add('ExtraNotes', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 255,
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['max' => 255])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => LapTime::class,
+        ]);
+    }
+}
