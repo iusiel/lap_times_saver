1116d356d25bd416470bada3c55de9ef8db438f6
Iusiel <iusiel@outlook.com>
2022-05-17 21:06:14 +0800

    Removed unnecessary function

--- assets/controllers/mychart_controller.js
+++ assets/controllers/mychart_controller.js
@@ -46,14 +46,4 @@ export default class extends Controller {
             }
         }
     }
-
-    _converUnixTimestampToReadable(value)
-    {
-        const date = new Date(value * 1000); // multiply by 1000 because this function uses milliseconds compared to return value from php strtotime() which uses seconds.
-        const hoursString = date.getUTCHours().toString().padStart(2,0);
-        const minutesString = date.getUTCMinutes().toString().padStart(2,0);
-        const secondsString = date.getUTCSeconds().toString().padStart(2,0);
-        const millisecondsString = date.getUTCMilliseconds().toString().padStart(3,0);
-        return `${hoursString}:${minutesString}:${secondsString}.${millisecondsString}`;
-    }
 }
