2b7a3bfeb3697a7b32f0aa16fd174c1c59d58eeb
Iusiel <iusiel@outlook.com>
2022-05-19 20:20:39 +0800

    Fixed issues detected by eslint

--- assets/js/services/SweetAlertOnDeleteAction.js
+++ assets/js/services/SweetAlertOnDeleteAction.js
@@ -2,27 +2,25 @@ import swal from 'sweetalert';
 
 const deleteForms = document.querySelectorAll('.form__delete');
 if (deleteForms.length > 0) {
-    deleteForms.forEach(form => {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            swal("Are you sure you want to do this?", {
-                buttons: {
-                    cancel: {
-                        text: "Cancel",
-                        visible: true,
-                        className: 'btn btn-secondary'
-                    },
-                    confirm: {
-                        className: 'btn btn-primary'
-                    },
-                  }
-            }).then((value) => {
-                if (value === true) {
-                    form.submit();
-                }
-            });
-        });
+  deleteForms.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      swal('Are you sure you want to do this?', {
+        buttons: {
+          cancel: {
+            text: 'Cancel',
+            visible: true,
+            className: 'btn btn-secondary',
+          },
+          confirm: {
+            className: 'btn btn-primary',
+          },
+        },
+      }).then((value) => {
+        if (value === true) {
+          form.submit();
+        }
+      });
     });
+  });
 }
-
-
