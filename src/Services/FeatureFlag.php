68db786dd5c96210b58655e5458cacdb1db2e287
Iusiel <iusiel@outlook.com>
2022-07-17 10:00:11 +0800

    Ignore warning by PHPMD

--- src/Services/FeatureFlag.php
+++ src/Services/FeatureFlag.php
@@ -4,6 +4,9 @@ namespace App\Services;
 
 class FeatureFlag
 {
+    /**
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
     public function check(string $featureFlag): bool
     {
         return !empty($_ENV[$featureFlag]) ? true : false;
